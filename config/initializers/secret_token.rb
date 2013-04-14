1bb8596a60c3456a88c896b9560a4d68d9cefbdc
Mason Chang <changmason@gmail.com>
2013-04-14 10:26:57 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newlibrary::Application.config.secret_token = '97007a72711ebac8ad97b3c29a8e511bb0ab09caad5bdf5e4ae54efee45439040684247185c10e390098cacb099202d0760b18d2ca552d82f211da7b2ca3f15e'
